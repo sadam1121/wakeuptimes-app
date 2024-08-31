7c8ee0bc772d0d702fbfd1abea25e6ba91458b2d
sadam1121 <sadam8@jhu.edu>
2024-08-30 21:55:20 -0400

    Initial Commit

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
-function calcSleepTime() {
+const clcBtn = document.getElementById("calc-btn");
+const refreshBtn = document.getElementById("refresh-btn");
+const returnBtn = document.getElementById("return-btn");
+const wakeUpHoursDiv = document.getElementById("wakeup-hours-div");
+const promptSection = document.getElementById("prompt-section");
+const imageContainer = document.getElementById("img-container");
+const resultSection = document.getElementById("result-section");
+
+clcBtn.addEventListener("click", calcWakeUpTimes);
+refreshBtn.addEventListener("click", calcWakeUpTimes);
+returnBtn.addEventListener("click", () => {
+  promptSection.classList.remove("hidden");
+  imageContainer.classList.remove("hidden");
+  resultSection.classList.add("hidden");
+});
+
+function calcWakeUpTimes() {
   const fallAsleepTime = new Date();
   fallAsleepTime.setMinutes(fallAsleepTime.getMinutes() + 14);
-  console.log(
-    "You will fall sleep at",
-    fallAsleepTime.toLocaleTimeString("en-US", {
-      timeStyle: "short",
-    })
-  );
-  
-   const wakeupTimesDiv = document.getElementById("wakeup-hours-div");
-    wakeupTimesDiv.innerHTML = "";
+
   const wakeUpTime = new Date(fallAsleepTime);
-  //const wakeUpTimes = [];
+  wakeUpHoursDiv.innerHTML = "";
   for (let i = 1; i <= 6; i++) {
     wakeUpTime.setMinutes(wakeUpTime.getMinutes() + 90);
     const wakeUpTimeString = wakeUpTime.toLocaleTimeString("en-US", {
       timeStyle: "short",
     });
-    // <div id = "cycle-1" class="cycle">12:15 AM </div>
-    //const sleepCycleDiv = document.getElementById(`cycle-${i}`);
-    const sleepCycleDiv = document.createElement("div");
-    sleepCycleDiv.setAttribute("id", `cycle-${i}`);
-    sleepCycleDiv.classList.add(`cycle`);
-    sleepCycleDiv.textContent = wakeUpTimeString;
-    //wakeUpTimes.push(wakeUpTimeString);
-
-    wakeupTimesDiv.appendChild(sleepCycleDiv);
+    const cycleDiv = document.createElement("div");
+    cycleDiv.classList.add("cycle");
+    cycleDiv.setAttribute("id", `cycle-${i}`);
+    cycleDiv.textContent = wakeUpTimeString;
+    wakeUpHoursDiv.appendChild(cycleDiv);
   }
-  
-  //console.log("Wake-up times:", wakeUpTimes.join(", "));
-  
-}
 
-const calcBtn = document.getElementById("calc-btn"); // this doesn't work but document is not in this environment, it is only in the browser
-calcBtn.onclick = calcSleepTime;
+  promptSection.classList.add("hidden");
+  imageContainer.classList.add("hidden");
+  resultSection.classList.remove("hidden");
+}
